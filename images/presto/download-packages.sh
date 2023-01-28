41fc9a2f288256ef4d1a24ec5f35dd402f1a9e6e
Izhar Firdaus <kagesenshi.87@gmail.com>
2023-01-28 18:35:01 +0800

    lock release

--- images/presto/download-packages.sh
+++ images/presto/download-packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
 PRESTO_VERSION=0.278.1
+RANGER_VERSION=2.3.0
+
 COMMONS_COMPRESS_VERSION=1.22
 SCRIPT_PATH=`realpath $0`
 HERE=`dirname $SCRIPT_PATH`
@@ -10,6 +12,7 @@ RANGER_PACKAGE=apache-ranger-${RANGER_VERSION}.tar.gz
 PRESTO_CLI_JAR=presto-cli-${PRESTO_VERSION}-executable.jar
 PRESTO_PACKAGE=presto-server-${PRESTO_VERSION}.tar.gz
 COMMONS_COMPRESS_JAR=commons-compress-${COMMONS_COMPRESS_VERSION}.jar
+RANGER_PLUGIN_PACKAGE=ranger-${RANGER_VERSION}-presto-plugin.tar.gz
 
 download () {
     if [ ! -f "$2" ];then
@@ -26,3 +29,5 @@ mkdir -p ${PKGDIR}
 download https://repo1.maven.org/maven2/com/facebook/presto/presto-server/${PRESTO_VERSION}/${PRESTO_PACKAGE} ${PKGDIR}/${PRESTO_PACKAGE}
 download https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/${PRESTO_VERSION}/${PRESTO_CLI_JAR} ${PKGDIR}/${PRESTO_CLI_JAR}
 download https://repo1.maven.org/maven2/org/apache/commons/commons-compress/${COMMONS_COMPRESS_VERSION}/${COMMONS_COMPRESS_JAR} ${PKGDIR}/${COMMONS_COMPRESS_JAR}
+
+cp ${HERE}/../ranger/release/${RANGER_PLUGIN_PACKAGE} ${PKGDIR}/${RANGER_PLUGIN_PACKAGE}
