2042f42cd17637ef16b91d6890d87ea1c3a7570d
Izhar Firdaus <kagesenshi.87@gmail.com>
2023-02-15 08:39:39 +0800

    added ability to define catalog through environment

---
+++ images/presto/create_catalog_config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os
+import sys
+from pprint import pprint
+
+if len(sys.argv) != 2:
+    print("Usage %s [DIRECTORY]" % sys.argv[0], file=sys.stderr)
+    sys.exit(1)
+
+outdir = sys.argv[1]
+if not os.path.exists(outdir):
+    print("Directory %s not found" % outdir)
+    sys.exit(1)
+    
+BASE_KEY='presto.catalog.'
+
+keys = os.environ.keys()
+
+catalogs = {}
+
+for k in keys:
+    if k.startswith(BASE_KEY):
+        catalog = k[len(BASE_KEY):].lower().split('.')[0]
+        setting = '.'.join(k[len(BASE_KEY):].lower().split('.')[1:])
+        catalogs.setdefault(catalog, {})
+        catalogs[catalog][setting] = os.environ[k]
+
+for catalog in catalogs:
+    with open("%s/%s.properties" % (outdir, catalog), 'w') as f:
+        for setting, value in catalogs[catalog].items():
+            val = '%s=%s' % (setting,value)
+            print(val)
+            f.write(val + '\n')      
--- images/presto/entrypoint.sh
+++ images/presto/entrypoint.sh
@@ -8,8 +8,11 @@ node.data-dir=$PRESTO_DATA_DIR
 node.environment=presto
 EOF
 
+
 if [ "$1" == "shell" ];then
     /bin/bash
+elif [ "$1" == "initcatalog" ];then
+    python3 /opt/presto/bin/create_catalog_config.py $PRESTO_CONF_DIR/catalog
 else
     python3 /opt/presto/bin/launcher.py "$@"
 fi
