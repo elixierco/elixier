12b413696470084936e7ebb16f09c273982f61e6
Izhar Firdaus <kagesenshi.87@gmail.com>
2023-04-02 01:59:17 +0800

    make pgsql configurable through environment vars

--- images/citus/entrypoint.sh
+++ images/citus/entrypoint.sh
@@ -5,10 +5,10 @@ BINDIR="/usr/pgsql-14/bin"
 SHAREDIR="/usr/pgsql-14/share"
 
 if [ "$1" == "start" ];then
+    python3 /opt/create_pgsql_conf.py ${PGDATA}/postgresql.conf
     $BINDIR/postgresql-14-check-db-dir ${PGDATA}
     cp /etc/pg_hba.conf ${PGDATA}/pg_hba.conf
     chmod og-rwx ${PGDATA}/pg_hba.conf
-    sed -i 's|logging_collector = on|logging_collector = off|g' ${PGDATA}/postgresql.conf
     $BINDIR/postmaster -D ${PGDATA} -h 0.0.0.0 "${@:2}"
 elif [ "$1" == "initdb" ];then
     if [ ! -f "${PGDATA}/PG_VERSION" ];then
